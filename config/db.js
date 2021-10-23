12fa43fb030210624003227e939d61fed6515d36
Vedant Pimple <60323634+vedantsp@users.noreply.github.com>
2021-10-23 23:25:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,1 @@
-const mongoose = require('mongoose')
-const colors = require('colors')
-const dotenv = require('dotenv')
 
-
-const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to the Database".green);
-}
-
-module.exports = connectDB;
